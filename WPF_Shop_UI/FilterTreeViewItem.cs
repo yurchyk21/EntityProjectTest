49cb5f1dc9a3a502c04437a75bf2fb58e6b9608f
yura <fanya21@gmail.com>
2018-01-31 21:03:58 +0200

    +delete +add

---
+++ WPF_Shop_UI/FilterTreeViewItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Shop_UI
+{
+    public class FilterTreeViewItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
--- WPF_Shop_UI/FilterWorkWindow.xaml.cs
+++ WPF_Shop_UI/FilterWorkWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using EntityProjectTest;
 using EntityProjectTest.Entities;
+using System.Transactions;
 
 namespace WPF_Shop_UI
 {
@@ -21,6 +22,8 @@ namespace WPF_Shop_UI
     /// </summary>
     public partial class FilterWorkWindow : Window
     {
+        List<ParentViewItem> parentItems = new List<ParentViewItem>();
+
         public FilterWorkWindow()
         {
             InitializeComponent();
@@ -28,36 +31,81 @@ namespace WPF_Shop_UI
 
         private void btnAddFilterName_Click(object sender, RoutedEventArgs e)
         {
-            using (EFContext context = new EFContext())
+            string name = txtFilterName.Text;
+            ParentViewItem newItem = new ParentViewItem()
             {
-                string name = txtFilterName.Text;
-                TreeViewItem newChild = new TreeViewItem();
-                newChild.Header = name;
-                TreeViewFilterName.Items.Add(newChild);
+                Name = name,
+                Id = Guid.NewGuid().ToString(),
+                children = new List<string>()
+            };
+            parentItems.Add(newItem);
 
-                var findFilter = context.FilterName.SingleOrDefault(f => f.Name == name);
-                if (findFilter != null)
-                //{
-                //    FilterName filterName = new FilterName()
-                //    {
-                //        Name = name
-                //    };
-                //    context.FilterName.Add(filterName);
-                //    context.SaveChanges();
-                //}
-                {
-                    MessageBox.Show("Filter already exists");
-                }
-            }
+            TreeViewItem newChild = new TreeViewItem();
+            newChild.Header = newItem;
+            TreeViewFilterName.Items.Add(newChild);
+
+            //using (EFContext context = new EFContext())
+            //{
+            //    string name = txtFilterName.Text;
+
+            //    var findFilter = context.FilterName.SingleOrDefault(f => f.Name == name);
+            //    if (findFilter != null)
+            //    {
+            //        FilterName filterName = new FilterName()
+            //        {
+            //            Name = name
+            //        };
+            //        context.FilterName.Add(filterName);
+            //        context.SaveChanges();
+            //        FilterTreeViewItem viewItem = new FilterTreeViewItem()
+            //        {
+            //            Id = filterName.Id,
+            //            Name = filterName.Name
+            //        };
+            //        TreeViewItem newCh = new TreeViewItem();
+            //        newCh.Header = viewItem;
+            //        TreeViewFilterName.Items.Add(newCh);
+            //    }
+            //    {
+            //        MessageBox.Show("Filter already exists");
+            //    }
+            //}
         }
 
         private void btnAddFilterValue_Click(object sender, RoutedEventArgs e)
         {
+
             string name = txtFilterValue.Text;
             TreeViewItem newChild = new TreeViewItem();
             newChild.Header = name;
-            var Parent = (TreeViewFilterName.SelectedItem as TreeView);
-            Parent.Items.Add(newChild);
+
+            var item = TreeViewFilterName.SelectedItem as TreeViewItem;
+
+            if (item.Header is ParentViewItem)
+            {
+                ParentViewItem newIt = item.Header as ParentViewItem;
+                parentItems.SingleOrDefault(p => p.Id == newIt.Id).children.Add(name);
+                item.Items.Add(newChild);
+            }
+
+
+
+            //var Parent = (TreeViewFilterName.SelectedItem as TreeView);
+            //Parent.Items.Add(newChild);
+
+
+            //string filterName = txtFilterValue.Text;
+            //ParentViewItem newItem = new ParentViewItem()
+            //{
+            //    Name = name,
+            //    Id = Guid.NewGuid().ToString()
+            //};
+            //parentItems.Add(newItem);
+
+            //TreeViewItem newChild = new TreeViewItem();
+            //newChild.Header = newItem;
+            //TreeViewFilterName.Items.Add(newChild);
+
         }
 
 
@@ -74,53 +122,134 @@ namespace WPF_Shop_UI
 
         private void btnGetParent_Click(object sender, RoutedEventArgs e)
         {
-            TreeViewItem item = TreeViewFilterName.SelectedItem as TreeViewItem;
-            if (item != null)
+            using (EFContext context = new EFContext())
             {
-                ItemsControl parent = GetSelectedTreeViewItemParent(item);
-
-                TreeViewItem treeitem = parent as TreeViewItem;
-                string MyValue = treeitem.Header.ToString();//Gets you the immediate parent
-                MessageBox.Show(MyValue);
+                TreeViewItem item = TreeViewFilterName.SelectedItem as TreeViewItem;
+                if (item != null)
+                {
+                    ItemsControl parent = GetSelectedTreeViewItemParent(item);
+                    if (parent != null)
+                    {
+                        TreeViewItem treeitem = parent as TreeViewItem;
+                        FilterTreeViewItem myVal = item.Header as FilterTreeViewItem;
+                        FilterTreeViewItem parentItem = treeitem.Header as FilterTreeViewItem;
+                        var deleteValue = context.FilterValue
+                            .SingleOrDefault(f => f.Id == myVal.Id);
+                        var deleteGroup = context.FilterNameGroup
+                            .SingleOrDefault(f => f.FilternameId == parentItem.Id);
+                        if (deleteValue != null)
+                        {
+                            context.FilterNameGroup.Remove(deleteGroup);
+                            context.FilterValue.Remove(deleteValue);
+                            context.SaveChanges();
+                        }
+                    }
+                }
             }
         }
-
-        private void txtFilterValue_Loaded(object sender, RoutedEventArgs e)
+        private void RefreshTreeView()
         {
+            TreeViewFilterName.Items.Clear();
+            TreeViewFilterName.DisplayMemberPath = "Name";
+            TreeViewFilterName.SelectedValuePath = "Id";
             using (EFContext context = new EFContext())
             {
                 var query = (from f in context.VFilterNameGroup.AsQueryable()
-                            select new {
-                                FNameId = f.FilterNameId,
-                                FName = f.FilterName,
-                                FValueId = f.FilterValueId,
-                                FValue = f.FilterValue
-                            });
-                var groupNames=(from f in query
-                               group f by new {
-                                   Id = f.FNameId,
-                                   Name=f.FName
-                               } into g
-                               orderby g.Key.Name
-                               select g);
+                             select new
+                             {
+                                 FNameId = f.FilterNameId,
+                                 FName = f.FilterName,
+                                 FValueId = f.FilterValueId,
+                                 FValue = f.FilterValue
+                             });
+                var groupNames = (from f in query
+                                  group f by new
+                                  {
+                                      Id = f.FNameId,
+                                      Name = f.FName
+                                  } into g
+                                  orderby g.Key.Name
+                                  select g);
                 foreach (var filterNames in groupNames)
                 {
-                    var FName = new
+                    var FName = new FilterTreeViewItem
                     {
                         Id = filterNames.Key.Id,
                         Name = filterNames.Key.Name
                     };
+
+                    TreeViewItem parent = new TreeViewItem();
+                    parent.Header = FName;
+                    TreeViewFilterName.Items.Add(parent);
+
+                    //MessageBox.Show(filterNames.Key.Name);
                     var fValues = from v in filterNames
                                   group v by new
                                   {
                                       Id = v.FValueId,
                                       Name = v.FValue,
                                   };
-                    MessageBox.Show(filterNames.Key.Name);
                     foreach (var filterValue in fValues)
                     {
-                        MessageBox.Show(filterValue.Key.Name);
+                        var FVal = new FilterTreeViewItem
+                        {
+                            Id = filterValue.Key.Id,
+                            Name = filterValue.Key.Name
+                        };
+                        parent.Items.Add(FVal);
+                        //TreeViewItem newChild = new TreeViewItem();
+                        //newChild.Header = filterValue.Key;
+                        //TreeViewFilterName.Items.Add(newChild);
 
+                        //   MessageBox.Show(filterValue.Key.Name);
+
+                    }
+                }
+            }
+        }
+
+        private void txtFilterValue_Loaded(object sender, RoutedEventArgs e)
+        {
+            RefreshTreeView();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            using (EFContext context = new EFContext())
+            {
+                foreach (var item in parentItems)
+                {
+                    if (item.children.Count > 0)
+                    {
+                        using (TransactionScope scope = new TransactionScope())
+                        {
+                            FilterName filtername = new FilterName()
+                            {
+                                Name = item.Name,
+                            };
+                            context.FilterName.Add(filtername);
+                            context.SaveChanges();
+                            foreach (var child in item.children)
+                            {
+                                FilterValue filterValue = new FilterValue()
+                                {
+                                    Name = child
+                                };
+                                context.FilterValue.Add(filterValue);
+                                context.SaveChanges();
+                                FilterNameGroup filterNameGroup = new FilterNameGroup()
+                                {
+                                    FilternameId = filtername.Id,
+                                    FilterValueId = filterValue.Id
+                                };
+                                context.FilterNameGroup.Add(filterNameGroup);
+                                context.SaveChanges();
+                            }
+                            scope.Complete();
+                            parentItems.Clear();
+
+                            RefreshTreeView();
+                        }
                     }
                 }
             }
